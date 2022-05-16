55a35c62b1601956b918af1d10acd45de1511e9b
ALLUK <lam83@bk.ru>
2022-05-17 00:42:05 +0500

    added some code

--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -31,5 +31,23 @@ namespace WebApplicationEmptyEx
         /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox Name;
+
+        /// <summary>
+        /// Button элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button;
+
+        /// <summary>
+        /// Label1 элемент управления.
+        /// </summary>
+        /// <remarks>
+        /// Автоматически создаваемое поле.
+        /// Для изменения переместите объявление поля из файла конструктора в файл кода программной части.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
